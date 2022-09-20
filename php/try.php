bb6275506edaed6ff9b3cc21243d5888705526c7
shamba-umar <shambaumarabedi@gmail.com>
2022-09-20 16:59:33 +0300

    git commit
    try.php

--- php/try.php
+++ php/try.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 }
 
 // sql to create table
-$sql = "CREATE TABLE registration  (
+$sql = "CREATE TABLE users  (
 id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 firstname VARCHAR(20) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
@@ -26,7 +26,7 @@ reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Table registration created successfully";
+  echo "Table users created successfully";
 } else {
   echo "Error creating table: " . $conn->error;
 }
