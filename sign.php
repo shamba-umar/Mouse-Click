f6ff788a4548ea2acfb3a49cc66649d9b6013715
shamba-umar <shambaumarabedi@gmail.com>
2022-09-21 16:59:34 +0300

    some changes

---
+++ sign.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            login
+        </title>
+        <link rel="stylesheet" type="text/css" href="mouseclick.css">
+    </head>
+    <body>
+<form action="login.php" method="post">
+<h2>login</h2>
+<?php if(isset($_GET['error'])) {  ?>
+<p class="error"> <?php echo $_GET['error'] ?> </p>
+<?php } ?>
+<label> user name</label>
+<input type="text" name="uname"placeholder="user Name"><br>
+<label> Email</label>
+<input type="text" name="email"placeholder="email"><br>
+<label>password</label>
+<input type="password" name="password " placeholder="password"><br>
+<p>By creating an account you agree to our <a href="terms&privacy.html" style="color:dodgerblue">Terms & Privacy</a>.</p>
+      
+          <div class="clearfix">
+            <button type="button" class="cancelbtn">Cancel</button>
+            <button type="submit" class="signupbtn">login</button>
+          </div>
+
+</form>
+
+
+    </body>
+</html>
